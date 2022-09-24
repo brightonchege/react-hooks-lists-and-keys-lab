09b87f6fdeff95020bb88d0f440294568d03c053
brighton <brightonspak@gmail.com>
2022-09-24 16:26:09 +0300

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,17 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const listlinks = links.map(item =><a key={item} href={`#${item}`}>{item}</a>)
+ /*function listt(links){
+  for(var i = 0; i < links.length ;i ++){
+    return <a href={`#${links[i]}`}>{links[i]}</a> 
+  }
+ }
+ */
+  return <nav>{listlinks}</nav>;
+    /* display an <a> tag for each link here */
 }
 
 export default NavBar;
+
+
